7667ac008bfdf232710e8892b224103041984296
DESKTOP-L97VA5M\oshiz <oshizawadai.1192@gmail.com>
2022-03-19 20:11:59 +0900

    Unitypackageとcsを追加

---
+++ CreatHLSLScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class CreatHLSLScript : EditorWindow
+{
+    static string path;
+
+    [MenuItem("Assets/Create/Shader/HLSLScript")]
+    private static void OpenWindow()
+    {
+        foreach (Object obj in Selection.GetFiltered(typeof(DefaultAsset), SelectionMode.DeepAssets))
+        {
+            if (obj is DefaultAsset)
+            {
+                path = AssetDatabase.GetAssetPath(obj);
+            }
+        }
+
+        path = EditorUtility.SaveFilePanelInProject("CreatHLSLScript", "NewShader", "hlsl", "", path);
+        if (!string.IsNullOrEmpty(path))
+        {
+            path = AssetDatabase.GenerateUniqueAssetPath(path);
+            File.WriteAllText(path,"");
+            AssetDatabase.Refresh();
+        }
+    }
+}
