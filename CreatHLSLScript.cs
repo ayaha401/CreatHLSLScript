6631b99fc3a2bb4c7710e3cbf53ba2180905e156
DESKTOP-L97VA5M\oshiz <oshizawadai.1192@gmail.com>
2022-03-19 20:23:46 +0900

    バージョン表記を追加

--- CreatHLSLScript.cs
+++ CreatHLSLScript.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections;
+﻿//====================================================================================================
+// v1.0.1
+// Twitter : @ayaha__401
+//====================================================================================================
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
